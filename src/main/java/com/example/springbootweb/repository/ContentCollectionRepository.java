2d674d34fe3ee9159cd7cc76a9c019c7397fef53
NithinLukose <nithinclukose12@gmail.com>
2024-01-10 09:06:22 +0530

    initial commit

---
+++ src/main/java/com/example/springbootweb/repository/ContentCollectionRepository.java
@@ -0,0 +1,37 @@
+package com.example.springbootweb.repository;
+
+import com.example.springbootweb.model.Content;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ContentCollectionRepository {
+
+    private final List<Content> contentList = new ArrayList<>();
+
+    public ContentCollectionRepository() {
+    }
+
+    public List<Content> findAll(){
+        return contentList;
+    }
+    public Optional<Content> findById(Integer id){
+        return contentList.stream().filter(c->c.id().equals(id)).findFirst();
+    }
+
+    public void save(Content content) {
+        contentList.removeIf(c->c.id().equals(content.id()));
+        contentList.add(content);
+    }
+
+    public boolean existsById(Integer id) {
+        return contentList.stream().filter(c->c.id().equals(id)).count() == 1;
+    }
+
+    public void delete(Integer id) {
+        contentList.removeIf(c->c.id().equals(id));
+    }
+}
